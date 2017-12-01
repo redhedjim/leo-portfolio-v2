4f80aba35b807bd535abb647a88d8cc5e1bfa09d
Leo <jamesleotucker@gmail.com>
2017-11-30 23:29:13 -0700

    added Procfile

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV !== 'production') {
 } else {
   app.use(express.static(`${__dirname}/dist`));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../dist/index.html'));
+    res.sendFile(path.join(__dirname, './dist/index.html'));
   });
 }
 const PORT = process.env.PORT || 8000;
