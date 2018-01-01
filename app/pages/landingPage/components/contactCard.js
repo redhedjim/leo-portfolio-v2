67200b6010dc656dd5acdcbf96a6a07b01f5ee00
Leo Tucker <leo@Leos-iMac.local>
2018-01-01 16:40:08 -0700

    fixed intro styling and contact card styling. added rookieUp logo

--- app/pages/landingPage/components/contactCard.js
+++ app/pages/landingPage/components/contactCard.js
@@ -3,6 +3,7 @@ import stackoverflowLogo from '../../../images/logos/logo-stackoverflow.png';
 import linkedinLogo from '../../../images/logos/logo-linkedin.png';
 import emailLogo from '../../../images/logos/logo-email.png';
 import githubLogo from '../../../images/logos/logo-github.png';
+import rookieUpLogo from '../../../images/logos/logo-rookieupBlack.jpg';
 
 const ContactCard = () => (
   <div className="contact-card">
@@ -11,6 +12,7 @@ const ContactCard = () => (
       <div className="social-media"><a href="mailto:jamesleotucker@gmail.com"><img role="presentation" className="img-responsive social-media-icon" src={emailLogo} /></a></div>
       <div className="social-media"><a href="http://www.linkedin.com/in/leo-tucker"><img role="presentation" className="img-responsive social-media-icon" src={linkedinLogo} /></a></div>
       <div className="social-media"><a href="https://stackoverflow.com/story/leotucker"><img role="presentation" className="img-responsive social-media-icon" src={stackoverflowLogo} /></a></div>
+      <div className="social-media"><a href="https://www.rookieup.com/leotucker/"><img role="presentation" className="img-responsive social-media-icon" src={rookieUpLogo} /></a></div>
     </div>
     <div className="contact-text"><p>jamesleotucker@gmail.com</p></div>
   </div>
--- app/pages/landingPage/components/introSection.js
+++ app/pages/landingPage/components/introSection.js
@@ -6,18 +6,17 @@ const IntroSection = () => (
       <div className="intro-left">
         <div>
           <p id="intro-text-1" className="title">Hi,</p>
-          <p id="intro-text-2" className="title">I'm Leo</p>
+          <p id="intro-text-2" className="title"> I'm Leo</p>
         </div>
       </div>
       <div className="intro-right intro-text-container">
         <p className="intro-header title">Software developer<span />Coding mentor</p>
         <p className="intro-text">I make high traffic enterprise 
-            applications with simple and easy to use UI. If you have to explain your UI to people, it 
-            probably isn't good. When I'm not making applications I am likely creating and hosting 
+            applications built with user experience in mind. When I'm not making applications I am likely creating and hosting 
             websites.
         </p>
         <p className="intro-text">Mentoring and coaching people on how to solve their coding problems 
-            is a passion of mine.
+            is a passion of mine. I love sharing my passion for learning with others.
         </p>
         <p className="intro-text">I'm from Louisiana, USA and currently live in Calgary, Canada.
             Poke around and check-out my projects as updates are always just around the corner.
